242102c0c03ffda60fca65d8aede8f5fc04f8b1a
veen190-raju <veenarajappa190@gmail.com>
2022-12-07 03:22:33 +0000

    modified some changes

--- file2.sh
+++ file2.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
+
 echo "enter the name to check"
 read name
+
 if [ -f $name ]
 then
-echo "content of a file"
+     echo "content of a file"
 cat $name
-echo "number of lines in a file" 
+     echo "number of lines in a file" 
 wc -l $name
 elif [ -d $name ]
 then
-echo "the directory contains a files as below"
+
+	echo "the directory contains a files as below"
 ls
+
 fi
 
 echo
+
 ls | wc -w
+
 fi
 
